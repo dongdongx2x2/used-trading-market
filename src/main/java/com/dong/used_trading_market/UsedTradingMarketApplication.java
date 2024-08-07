ee9edbfb9050e23f42493404781e2b96429e6f69
donghyun <kdhdh001@naver.com>
2024-08-07 14:54:14 +0900

    feat: initial project

---
+++ src/main/java/com/dong/used_trading_market/UsedTradingMarketApplication.java
@@ -0,0 +1,13 @@
+package com.dong.used_trading_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedTradingMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedTradingMarketApplication.class, args);
+	}
+
+}
